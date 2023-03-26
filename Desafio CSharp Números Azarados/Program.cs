7cf8114035bbeefb56c480b1a34ef902fcd5fc57
carlosmg2005 <carlosmg2005@msn.com>
2023-03-26 18:53:00 -0300

    Adicionar arquivos de projeto.

---
+++ Desafio CSharp Números Azarados/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_CSharp_Números_Azarados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numero = Console.ReadLine();
+            string contem = "13";
+
+            if (numero.Contains(contem))
+            {
+                var malaSuerte = $"{numero} es de Mala Suerte";
+                Console.WriteLine(malaSuerte);
+            }
+            else
+            {
+                var suerte = $"{numero} NO es de Mala Suerte";
+                Console.WriteLine(suerte);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
